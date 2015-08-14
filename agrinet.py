12fefe81c30dcf917e53e76679d80dfc1de537f5
Inzamam Rahaman <InzamamRahaman@users.noreply.github.com>
2015-08-14 14:01:43 -0400

    Updated agrinet.py in attempt to run on heroku

--- agrinet.py
+++ agrinet.py
@@ -392,5 +392,5 @@ def login():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host = '127.0.0.1', port = port)
-	#app.run(host='0.0.0.0', port=port)
+	#app.run(host = '127.0.0.1', port = port)
+	app.run(host='0.0.0.0', port=port)
